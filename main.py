d6addc96455b4202da30ccfb7f90da724d76fecc
juhani.kuru <juhanialeksikuru@gmail.com>
2025-04-28 09:36:06 +0300

    -add json route - add comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 # luodaan flask serveri tässä
 # annetaan sille nimeksi skriptin nimi __name__-muuttuja on oletuksena python skriptillä ja sen arvo on skriptin nimi
@@ -12,6 +12,24 @@ app = Flask(__name__)
 # indeksiä sanotaan siksi routehandleriksi
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/v1/users')
+def get_users():
+    # flaskilla ei ole pakko käyttää html:ää vastausten content-typenä
+    # sillä voi lähettää dataa esim. myös json-formaatissa
+    # pythonin dict-tietotyyppi sopii jsonin lähetykseen hyvin, koska muoto jsonissa ja dictionaryssa on sama
+    """
+
+    {'avain': 'arvo', 'avain2': 1, 'bands': ['Gorgoroth', 'Torture Killer', 'Dimmu Borgir', 'Cradle Of Filth', 'Behemoth', 'Immortal', 'Watain']}
+
+    :return:
+    """
+    # jsonify-funktio muuttaa http-responsen tyypin application/jsoniksi => content-type: application/json
+    # jotta selain ymmärtää vastauksen olevan html:n sijasta jsonia
+    return jsonify([
+        {'id': 1, 'username': 'Juhani', 'role:': 'normal-user'},
+    ])
 
 
 
